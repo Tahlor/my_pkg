a87ecdd37646209a52294725d3cb51b07f9a5035
Taylor Archibald <tahlor@gmail.com>
2018-06-15 12:26:45 -0600

    test

---
+++ my_pkg/tests/test_my_pkg.py
@@ -0,0 +1,12 @@
+"""
+Tests the mathematical functions defined in my_pkg/trail.py
+"""
+
+import pytest
+
+def test_square():
+    """Tests the squaring function"""
+
+    from my_pkg.trial import square
+
+    assert 4 == square(2)
